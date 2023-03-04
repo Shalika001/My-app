71388241a3a1e9fee99967500e3d4f4b6d0ff6f4
Shalika001 <shalikaupamada@gmail.com>
2023-03-04 23:28:38 +0530

    create stateless widget

--- First_project/lib/main.dart
+++ First_project/lib/main.dart
@@ -1,28 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My first app'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
-          'Hello Shalika!!',
-        style: TextStyle(
-          fontSize: 30.0,
-          fontWeight: FontWeight.bold,
-          color: Colors.cyanAccent[700],
-          letterSpacing: 2.0,
-          fontFamily:
-        )
-      ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {  },
-      child: Text('Click'),
-      backgroundColor: Colors.red[600],
-    ),
-  ),
+  home: Home(),
 ));
+
+class Home extends StatelessWidget {
+  //const Home({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My first app'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
+            'Hello Shalika!!',
+            style: TextStyle(
+              fontSize: 30.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.cyanAccent[700],
+              letterSpacing: 2.0,
+              fontFamily: 'ShantellSans',
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
